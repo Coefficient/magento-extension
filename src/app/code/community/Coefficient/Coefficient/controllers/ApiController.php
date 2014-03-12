d617351009a7eebb8b2033acf6b47c4ea83167b5
Skyler Slade <jsslade@gmail.com>
2014-03-12 11:11:35 -0400

    Re-enabled authorization checking. HTTPS check is still disabled.

--- src/app/code/community/Coefficient/Coefficient/controllers/ApiController.php
+++ src/app/code/community/Coefficient/Coefficient/controllers/ApiController.php
@@ -61,15 +61,6 @@ class Coefficient_Coefficient_ApiController extends Mage_Core_Controller_Front_A
         return true;
     }
 
-    #public function testAction()
-    #{
-    #    #$orderItem = Mage::getModel('sales/order_item')->load($orderItem->getId());
-    #    $collection = Mage::getModel('sales/order_item')->getCollection();
-    #    foreach ($collection as $item) {
-    #        print_r($item->getData());
-    #    }
-    #}
-
     public function versionAction()
     {
         if (!$this->isAuthorized()) {
@@ -81,18 +72,13 @@ class Coefficient_Coefficient_ApiController extends Mage_Core_Controller_Front_A
 
     public function customersAction()
     {
-        #if (!$this->isAuthorized()) {
-        #    return $this;
-        #}
+        if (!$this->isAuthorized()) {
+            return $this;
+        }
 
         $collection = Mage::getResourceModel('customer/customer_collection')
                ->addNameToSelect()
                ->addAttributeToSelect('*')
-               #->addAttributeToSelect('firstname')
-               #->addAttributeToSelect('lastname')
-               #->addAttributeToSelect('email')
-               #->addAttributeToSelect('created_at')
-               #->addAttributeToSelect('group_id')
                ->joinAttribute('billing_postcode', 'customer_address/postcode', 'default_billing', null, 'left')
                ->joinAttribute('billing_city', 'customer_address/city', 'default_billing', null, 'left')
                ->joinAttribute('billing_region', 'customer_address/region', 'default_billing', null, 'left')
@@ -123,9 +109,9 @@ class Coefficient_Coefficient_ApiController extends Mage_Core_Controller_Front_A
 
     public function ordersAction()
     {
-        #if (!$this->isAuthorized()) {
-        #    return $this;
-        #}
+        if (!$this->isAuthorized()) {
+            return $this;
+        }
 
         $collection = Mage::getModel('sales/order')->getCollection()
             ->addFieldToFilter('status', 'complete')
